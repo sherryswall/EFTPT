11e603f0054c648db389a46ad607b87a6a29a1d1
sherrynaeem <sherrynaeem2@hotmail.com>
2018-04-15 21:00:43 +1000

    Add project files.

---
+++ ConsoleApp2/Helper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Helper
+    {
+        public void AddChildOne()
+
+        {
+            AddChild(new ChildOne() { childPropOne = "child one property", comments = "sherry adding cost", texting = "hey hey" });
+        }
+
+        public void AddChildTwo()
+        {
+            AddChild(new childTwo() { Cost = 10, comments = "sherry adding cost for child two", texting = "texting 2" });
+        }
+        public void ReadChildrenTwo()
+        {
+
+            Console.WriteLine(ReadChildren<childTwo>().FirstOrDefault().comments);
+        }
+
+        public void AddChild<T>(T rr) where T : Parent
+        {
+            using (var context = new MovieDBEntities())
+            {
+                context.Parents.Add(rr);
+                try
+                {
+                    context.SaveChanges();
+
+                }
+                catch (Exception ex)
+                {
+
+                    throw ex;
+                }
+
+            }
+        }
+
+        public List<T> ReadChildren<T>() where T : Parent
+        {
+            List<T> tts = new List<T>();
+            using (var context = new MovieDBEntities())
+            {
+                tts = context.Parents.OfType<T>().ToList();
+            }
+            return tts;
+        }
+
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Helper h = new Helper();
+            h.AddChildOne();
+            h.AddChildTwo();
+
+            h.ReadChildrenTwo();
+
+        }
+    }
+}
